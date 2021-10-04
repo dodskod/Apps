e1e307b29a686640a2bd700ae5a7e11088b51704
WillianHuit <80606833+Seito98@users.noreply.github.com>
2021-10-04 10:33:44 -0600

    version 1.0
    New design for the main page

---
+++ Info/main.js
@@ -0,0 +1,31 @@
+let listGames =[
+		"Float Boo",
+		"Pixel Dado",
+		"Pixel Moneda"
+	];
+let canShowing = false;
+function starts(){
+	//appName
+	let appName = getName("appName");
+	for (var i = listGames.length - 1; i >= 0; i--) {
+		if (listGames[i] == appName) {
+			canShowing = true;
+			break;
+		}
+	}
+	if (canShowing) {
+		document.getElementById("appName").innerHTML = appName;
+	}else{
+		//realApp
+		document.getElementById("realApp").className = "hideSection";
+		document.getElementById("fakeApp").className = "showSection";
+	}
+	
+}
+
+function getName(name){
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	let regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+	let results = regex.exec(location.search);
+	return results === null ? "Float Boo" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
